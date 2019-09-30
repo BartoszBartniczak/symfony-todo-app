d78ca3e92116f1a0dacfd69662695de16ddd5d04
Bartosz Bartniczak <bartoszbartniczak@gmail.com>
2019-09-30 20:12:46 +0200

    Task unit tests

---
+++ tests/Entity/TaskTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bartosz
+ */
+
+namespace App\Tests\Entity;
+
+use App\Entity\Status\Status;
+use App\Entity\Task;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+
+    /**
+     * @covers \App\Entity\Task::__construct
+     * @covers \App\Entity\Task::getId
+     * @covers \App\Entity\Task::getTitle
+     * @covers \App\Entity\Task::getDescription
+     * @covers \App\Entity\Task::getStatus
+     * @covers \App\Entity\Task::changeTitle
+     * @covers \App\Entity\Task::changeDescription
+     * @covers \App\Entity\Task::changeStatus
+     */
+    public function testConstructorGettersAndSetters(){
+        $status = new Status(Status::TO_DO);
+        $task = new Task('fd229975-bc53-4646-836f-c700e1bcf147', 'Title', 'description', $status);
+
+        $this->assertSame('fd229975-bc53-4646-836f-c700e1bcf147', $task->getId());
+        $this->assertSame('Title', $task->getTitle());
+        $this->assertSame('description', $task->getDescription());
+        $this->assertSame($status, $task->getStatus());
+
+        $task->changeTitle('New title');
+        $this->assertSame('New title', $task->getTitle());
+
+        $task->changeDescription('New description');
+        $this->assertSame('New description', $task->getDescription());
+
+        $newStatus = new Status(Status::IN_PROGRESS);
+        $task->changeStatus($newStatus);
+        $this->assertSame($newStatus, $task->getStatus());
+    }
+
+}
